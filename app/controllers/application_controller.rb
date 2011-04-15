cbcd3a1d91c0a293ae7c8d51fc1b60059df7c623
tingy <victor.lin@storysensecomputing.com>
2011-04-15 13:26:13 +0800

    add after_sign_up_path_for in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,16 @@ class ApplicationController < ActionController::Base
 
 private
 
-def after_sign_out_path_for(resource_or_scope)
-    home_url
-end
+    def after_sign_up_path_for(resource_or_scope)
+        home_url
+    end
 
-def after_sign_in_path_for(resource_or_scope)
-    home_url
-end
+    def after_sign_in_path_for(resource_or_scope)
+        home_url
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+        home_url
+    end
 
 end
