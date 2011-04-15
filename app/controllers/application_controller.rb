6bc902a56c3f3d331bf68c4873cabf2743b1f740
tingy <victor.lin@storysensecomputing.com>
2011-04-15 12:55:00 +0800

    sotransfer the db from sqlite to mysql. </n> Solve the routing problem. redirect to /home instead of /

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+private
+
+def after_sign_out_path_for(resource_or_scope)
+    home_url
+end
+
+def after_sign_in_path_for(resource_or_scope)
+    home_url
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ CliikUIDB::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
+  match '/home', :to => 'home#index', :as => "home"
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
@@ -50,7 +51,7 @@ CliikUIDB::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => "user#index"
+  # root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
