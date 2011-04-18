cc3d1d54726e2fb0c7c568521cff32a4150808a5
tingy <victor.lin@storysensecomputing.com>
2011-04-18 15:50:31 +0800

    Fix the sign up redirect problem. Using RegistrationsController.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 CliikUIDB::Application.routes.draw do
-  devise_for :users
+#  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
